66a704b6a8068e1e6fa5e14ecf796ecdc958099b
Tyler <deathnuts90@gmail.com>
2024-04-01 14:00:41 -0500

    psuedocode on main tasks

--- main.py
+++ main.py
@@ -1,5 +1,20 @@
-print("hello world")
+# In this project you will create an online banking program. Users need to 
+# have an account number and PIN to identify themselves as owners of an 
+# account. Once users get into the system they will have standard options: 
+# check balance, deposit, and withdraw. Additionally, a new user or bank 
+# administrator can also create a new account, close account, and modify 
+# an account (such as edit name, PIN, or any other personal identification 
+# required to open an account).
 
-variable = 100000
 
-print(variable)
+#introduction (welcome message, ask account number and PIN)
+
+
+
+
+#if new user: create, close, or modify an account
+
+
+
+
+#present standard options (check balance, deposit, and withdraw
