fe2bbf32680f277c40b61c76d1f756827c9f1603
Tyler <deathnuts90@gmail.com>
2024-04-01 14:26:25 -0500

    intro and started input for new account

--- main.py
+++ main.py
@@ -8,13 +8,20 @@
 
 
 #introduction (welcome message, ask account number and PIN)
+def intro():
+    print("Welcome to the Elite Bank, please input your account number and PIN to access your account.")
+    print("If you do not have an account, input 'new' to create one.\n")
 
-
+    return input("")
 
 
 #if new user: create, close, or modify an account
 
+def creating_acc():
+    print('hello')
 
+#present standard options (check balance, depositnew
+#  and withdraw
 
-
-#present standard options (check balance, deposit, and withdraw
+if intro() == "new":
+    creating_acc()
